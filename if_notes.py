8b29af3008c204d284a2b82a94b3dbd325fb9562
FaluckH <faluckhafiz@gmail.com>
2024-11-11 12:21:50 +0000

    if list variable list notes added

---
+++ if_notes.py
@@ -0,0 +1,76 @@
+# music ="classical"
+
+# if music == "classical"
+#     print("Oh no, not classical again")
+
+# music = "no music"
+
+# if music == "classical":
+#     print("Oh no, not classical again!")
+# elif music == "no music":
+#     print("Alexa, play some music!")
+# elif music == "britpop":
+#     print ("my second favourite! Turn it up!")
+# else:
+#     print("I've not heard this before!")
+
+
+# if music == "Oasis":
+#     print("The best!")
+# elif music !="britpop":
+#     print("I want to listen to britpop")
+# else:
+#     print("Yay! Britpop!")
+
+# if age = int(input("What is your"))"18"
+#     print("I can serve you")
+# elif age !="18":
+#     print("You aren't old enough!")
+  
+
+
+
+# age = int(input("Enter your age: "))
+# location = input("Enter your location: ").strip().lower()
+ 
+# if age >= 18 and location == "uk":
+#     print("Yes I can serve you")
+# elif age >= 18:
+#     print("I can serve you only if you're in the UK")
+# else:
+#     print("You aren't old enough")
+
+# day = "Saturday"
+
+# if day == "Saturday":
+#     print("A day off!")
+# elif day == "Sunday":
+#     print("A day off!")
+# else:
+#     print("Off to work I go")
+
+# day = "Monday"
+# bank_holiday = True
+
+# if day == "Saturday" or day == "Sunday" or bank_holiday==True:
+#     print("A day off!")
+# else:
+#     print("Off to work I go.")
+
+------------------------Activity 1 -----------------------
+
+# password = input ("Enter your password!")
+# if len(password) < 8:
+#     print("The password is too short")
+# else:
+#     print(f"The password is {password}")
+ 
+----------------Activity 2 -------------------------
+
+num = 8
+if num %3 == 0 or num %5 == 0
+  print ("This number is divisible by 3 or 5.")
+else:
+  print ("This number is acceptable, password")
+
+  ---------------Activity 3 ----------------------
---
+++ variables_notes.py
@@ -0,0 +1,33 @@
+# my_name = "Faluck"
+# my_age = 40
+# is_a_learner =False
+
+# print(my_name)
+
+# fav_drink = "hot chocolate"
+
+# print (fav_drink)
+
+# print ("My favourite drink is", fav_drink)
+
+# # print(my_name + "is" + my_age + "their favourite drink is " + fav_drink) 
+
+# user_name = input ("Type your name here: ")
+# print (f"hello, {user_name}")
+
+# print(type(user_name))
+
+# print("Type in two numbers to multiply them")
+
+# num1 = int(input("Type in your first number: "))
+# num2 = int(input("Type in your second number: "))
+
+# print
+# (num1*num2)
+
+# balance = 100
+# amount_to_withdraw = int(input("Type in your first number:  "))
+# print(balance)
+# balance = balance - amount_to_withdraw
+# print(balance) 
+
